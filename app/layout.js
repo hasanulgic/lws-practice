458cc83cae5aabd42bc967e1ae1f5484e80cd1e7
hasanulgic <hasanulgic@gmail.com>
2024-05-15 18:32:58 +0600

    html converted in next

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Footer from "@/componets/Home/Footer";
+import Header from "@/componets/Home/Header";
+import Navbar from "@/componets/Home/Navbar";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Header />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
